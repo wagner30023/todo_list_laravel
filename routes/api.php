8f3fb96335f63bf24cdb1732ec49cb566ebc6c21
wagner <wagner30023@gmail.com>
2022-03-08 11:28:15 -0300

    implementação da biblioteca de autenticação sanctum

--- routes/api.php
+++ routes/api.php
@@ -3,16 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApiController;
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\AuthController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -23,7 +14,14 @@ Route::get('/ping',function (Request $request) {
     return ['pong' => true]; 
 });
 
-Route::post('/todo',[ApiController::class,'createTodo']);
+Route::get('/unauthenticated',function (Request $request) {
+    return ['error' => 'usuário não logado'];
+})->name('login');
+
+Route::post('/user',[AuthController::class,'create']);
+Route::post('/auth',[AuthController::class,'login']);
+
+Route::middleware('auth:sanctum')->post('/todo',[ApiController::class,'createTodo']);
 Route::get('/todos',[ApiController::class,'readAllTodos']);
 Route::get('/todos/{id}',[ApiController::class,'readTodo']);
 Route::put('/todo/{id}',[ApiController::class,'updateTodo']);
